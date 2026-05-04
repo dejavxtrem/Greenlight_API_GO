23bfd6ec61112cec696135627469100e8963feb7
Dejay Odujoko <dejavxtrem@hotmail.com>
2026-05-03 21:49:26 -0400

    Recover panic middleware

---
+++ cmd/api/middleware.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a deferred function (which will always be run in the event
+		// of a panic).
+		defer func() {
+			// Use the built-in recover() function to check if a panic occurred.
+			// If a panic did happen, recover() will return the panic value. If
+			// a panic didn't happen, it will return nil.
+			pv := recover()
+
+			if pv != nil {
+				// Set a "Connection: close" header on the response.
+				w.Header().Set("Connection", "close")
+				// Call the app.serverError helper method to return a 500
+				// Internal Server response.
+				app.serverErrorResponse(w, r, fmt.Errorf("%v", pv))
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}

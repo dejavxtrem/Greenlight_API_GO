91d4e2d41d4dcb473b2caa54d1930f2176375d70
Dejay Odujoko <dejavxtrem@hotmail.com>
2026-06-12 00:35:13 -0400

    added validators

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/dejavxtrem/Greenlight_API/internal/data"
+	"github.com/dejavxtrem/Greenlight_API/internal/validator"
 	// New import
 )
 
@@ -26,6 +27,21 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		Genres  []string     `json:"genres"`
 	}
 
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		//app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy the values from the input struct to a new Movie struct.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
 	// Initialize a new json.Decoder instance which reads from the request body, and
 	// then use the Decode() method to decode the body contents into the input struct.
 	// Importantly, notice that when we call Decode() we pass a *pointer* to the input
@@ -33,12 +49,38 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	// we use our generic errorResponse() helper to send a 400 Bad Request response
 	// with the error message to the client.
 	//err := json.NewDecoder(r.Body).Decode(&input)
-	err := app.readJSON(w, r, &input)
-	if err != nil {
-		//app.errorResponse(w, r, http.StatusBadRequest, err.Error())
-		app.badRequestResponse(w, r, err)
+
+	// Initialize a new Validator instance.
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
+	// v.Check(input.Title != "", "title", "must be provided")
+	// v.Check(len(input.Title) <= 500, "title", "must not be more than 500 bytes long")
+	// v.Check(input.Year != 0, "year", "must be provided")
+	// v.Check(input.Year >= 1888, "year", "must be greater than 1888")
+	// v.Check(input.Year <= time.Now().Year(), "year", "must not be in the future")
+
+	// v.Check(input.Runtime != 0, "runtime", "must be provided")
+	// v.Check(input.Runtime > 0, "runtime", "must be a positive integer")
+
+	// v.Check(input.Genres != nil, "genres", "must be provided")
+	// v.Check(len(input.Genres) >= 1, "genres", "must contain at least 1 genre")
+	// v.Check(len(input.Genres) <= 5, "genres", "must not contain more than 5 genres")
+
+	// // Note that we're using the Unique helper in the line below to check that all
+	// // values in the input.Genres slice are unique.
+	// v.Check(validator.Unique(input.Genres), "genres", "must not contain duplicate values")
+
+	// Use the Valid() method to see if any of the checks failed. If they did, then use
+	// the failedValidationResponse() helper to send a response to the client, passing
+	// in the v.Errors map.
+	// if !v.Valid() {
+	// 	app.failedValidationResponse(w, r, v.Errors)
+	// 	return
+	// }
 
 	fmt.Fprintf(w, "%+v\n", input)
 }
